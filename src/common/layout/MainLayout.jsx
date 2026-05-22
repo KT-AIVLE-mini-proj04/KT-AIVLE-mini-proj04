2e96e79024a89e6f5b8c382686159f32dade6282
jhdgo1225 <parkjh99365@gmail.com>
2026-05-22 18:18:20 +0900

    STYLE: footer position fixed  제거
    브라우저 높이를 overflow 해도 footer가 보이는 문제 해소
    페이지가 브라우저 높이을 overflow 하지 않더라도 footer가 아래로 배치되도록 수정

--- src/common/layout/MainLayout.jsx
+++ src/common/layout/MainLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "../../App.css";
 
 function MainLayout() {
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      <Outlet />
+      <main className="main-content">
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
