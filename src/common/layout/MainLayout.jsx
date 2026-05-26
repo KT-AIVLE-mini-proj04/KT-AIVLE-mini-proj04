339b534bfc6b9d65b128b5efb98608cdf7a22d7d
jhdgo1225 <parkjh99365@gmail.com>
2026-05-26 13:04:30 +0900

    FEAT: Loading 컴포넌트 구현
    페이지 방식 X

--- src/common/layout/MainLayout.jsx
+++ src/common/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Loading from "../components/Loading";
 import "../../App.css";
 
 function MainLayout() {
   return (
     <div className="layout-container">
+      <Loading />
       <Header />
       <main className="main-content">
         <Outlet />
