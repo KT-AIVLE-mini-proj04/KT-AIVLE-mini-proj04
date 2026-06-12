d1a4d6d1504c8d5ff865bb579b3b2c03bd6598a8
hnil <jjang981226@naver.com>
2026-06-12 15:05:34 +0900

    protect router

--- src/common/components/ProtectedRoute.jsx
+++ src/common/components/ProtectedRoute.jsx
@@ -1,15 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useOutletContext } from "react-router-dom";
 import { getAccessToken } from "@utils/authStore";
 
 function ProtectedRoute() {
   const token = getAccessToken();
+  const outletContext = useOutletContext();
 
   if (!token) {
     alert("로그인 이후 접속해주세요.");
     return <Navigate to="/login" replace />;
   }
 
-  return <Outlet />;
+  return <Outlet context={outletContext} />;
 }
 
 export default ProtectedRoute;
